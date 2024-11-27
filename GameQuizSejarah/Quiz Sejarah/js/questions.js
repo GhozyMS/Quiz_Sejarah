4a19d01a55c9bd152958a5f7d0a407907e37f2f7
GhozyMS <156147082+GhozyMS@users.noreply.github.com>
2024-11-27 17:11:05 +0700

    Add files via upload

---
+++ GameQuizSejarah/Quiz Sejarah/js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Apa latar belakang utama dilakukannya Perjanjian Linggarjati?",
+    answer: "Upaya penyelesaian konflik antara Indonesia dan Belanda setelah Proklamasi",
+    options: [
+      "Keinginan Inggris mendominasi Asia Tenggara",
+      "Keinginan Belanda menguasai Indonesia sepenuhnya",
+      "Upaya penyelesaian konflik antara Indonesia dan Belanda setelah Proklamasi",
+      "Konflik antara Jepang dan Belanda"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Kapan Perjanjian Linggarjati ditandatangani?",
+    answer: "15 November 1946",
+    options: [
+      "15 Desember 1946",
+      "15 November 1946",
+      "10 November 1946",
+      "20 November 1946"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Siapa yang mewakili Indonesia dalam Perjanjian Linggarjati?",
+    answer: "Sutan Sjahrir",
+    options: [
+      "Moh. Hatta",
+      "Sutan Sjahrir",
+      "Tan Malaka",
+      "Soekarno"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Isi utama Perjanjian Linggarjati adalah?",
+    answer: "Belanda mengakui wilayah RI meliputi Jawa, Sumatra, dan Madura",
+    options: [
+      "Indonesia menjadi bagian dari Persemakmuran Belanda",
+      "Belanda mengakui wilayah RI meliputi Jawa, Sumatra, dan Madura",
+      "Belanda mengakui kedaulatan penuh Indonesia",
+      "Pembentukan Republik Indonesia Serikat (RIS)"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Perjanjian Renville dilakukan di atas kapal perang bernama?",
+    answer: "USS Renville",
+    options: [
+      "USS Renville",
+      "USS Franklin",
+      "HMS Sussex",
+      "KRI Irian"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Siapa yang mewakili Indonesia dalam Perjanjian Renville?",
+    answer: "Amir Sjarifuddin",
+    options: [
+      "Amir Sjarifuddin",
+      "Moh. Hatta",
+      "Sutan Sjahrir",
+      "Soekarno"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Apa isi utama Perjanjian Renville?",
+    answer: "Garis demarkasi ditetapkan sesuai garis Van Mook",
+    options: [
+      "Penghapusan Republik Indonesia Serikat",
+      "Pengakuan RI meliputi seluruh wilayah Hindia Belanda",
+      "Garis demarkasi ditetapkan sesuai garis Van Mook",
+      "Pengembalian wilayah Indonesia yang dikuasai Belanda"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Apa dampak Perjanjian Renville terhadap wilayah Republik Indonesia?",
+    answer: "Wilayah RI menyempit dan hanya meliputi Yogyakarta",
+    options: [
+      "Wilayah Indonesia bertambah luas",
+      "Wilayah RI tetap meliputi Jawa, Sumatra, dan Madura",
+      "Belanda mengembalikan semua wilayah RI",
+      "Wilayah RI menyempit dan hanya meliputi Yogyakarta"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Apa peran Komisi Tiga Negara (KTN) dalam Perjanjian Renville?",
+    answer: "Memediasi negosiasi antara Indonesia dan Belanda",
+    options: [
+      "Menyusun konstitusi baru untuk Indonesia",
+      "Memediasi negosiasi antara Indonesia dan Belanda",
+      "Membantu Belanda menguasai Indonesia",
+      "Membantu membentuk Republik Indonesia Serikat"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Apa alasan Belanda menginginkan Perjanjian Renville?",
+    answer: "Mempertahankan kontrol atas wilayah Indonesia",
+    options: [
+      "Menghapus Republik Indonesia Serikat",
+      "Mempertahankan kontrol atas wilayah Indonesia",
+      "Mengembalikan semua wilayah kepada Indonesia",
+      "Menyerahkan kedaulatan sepenuhnya kepada Indonesia"
+    ]
+  }
+];
